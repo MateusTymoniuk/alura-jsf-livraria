d4197df016bd3d2c91a698d49c49d6a2581bbda7
Mateus Tymoniuk <tymoniuk.m@gmail.com>
2017-08-26 20:29:34 -0300

    Fim do curso de Primefaces

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -12,6 +12,7 @@ import javax.faces.validator.ValidatorException;
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
+import br.com.caelum.livraria.modelo.LivroDataModel;
 import br.com.caelum.livraria.util.RedirectView;
 
 @ManagedBean
@@ -21,6 +22,8 @@ public class LivroBean {
 	private Livro livro = new Livro();
 	private Integer livroId;
 	private Integer autorId;
+	private List<Livro> livros;
+	private LivroDataModel livroDataModel = new LivroDataModel();
 	
 	public Integer getLivroId() {
 		return livroId;
@@ -33,13 +36,17 @@ public class LivroBean {
 	public Livro getLivro() {
 		return livro;
 	}
-	
+
 	public void setLivro(Livro livro) {
 		this.livro = livro;
 	}
 
 	public List<Livro> getLivros() {
-		return new DAO<Livro>(Livro.class).listaTodos();
+		DAO<Livro> dao = new DAO<Livro>(Livro.class);
+		if (this.livros == null) {
+			this.livros = dao.listaTodos();
+		}
+		return livros;
 	}
 
 	public List<Autor> getAutores() {
@@ -58,12 +65,16 @@ public class LivroBean {
 		this.autorId = autorId;
 	}
 
+	public LivroDataModel getLivroDataModel() {
+		return livroDataModel;
+	}
+
 	public void gravarAutor() {
 		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
 		this.livro.adicionaAutor(autor);
 		System.out.println("Escrito por: " + autor.getNome());
 	}
-	
+
 	public RedirectView formAutor() {
 		return new RedirectView("autor");
 	}
@@ -71,7 +82,7 @@ public class LivroBean {
 	public void carregarlivroPeloId() {
 		this.livro = new DAO<Livro>(Livro.class).buscaPorId(livroId);
 	}
-	
+
 	public void gravar() {
 		System.out.println("Gravando livro " + getLivro().getTitulo());
 
@@ -82,26 +93,29 @@ public class LivroBean {
 			return;
 		}
 
-		if(this.livro.getId() == null) {
-			new DAO<Livro>(Livro.class).adiciona(this.livro);
+		DAO<Livro> dao = new DAO<Livro>(Livro.class);
+		if (this.livro.getId() == null) {
+			dao.adiciona(this.livro);
+			this.livros = dao.listaTodos();
 		} else {
-			new DAO<Livro>(Livro.class).atualiza(this.livro);
+			dao.atualiza(this.livro);
 		}
 
 		this.livro = new Livro();
 	}
-	
+
 	public void remover(Livro livro) {
 		System.out.println("Removendo livro " + livro);
 		new DAO<Livro>(Livro.class).remove(livro);
 	}
-	
+
 	public void removeAutorDoLivro(Autor autor) {
 		System.out.println("Removendo autor " + autor);
 		this.livro.removerAutor(autor);
 	}
 
-	public void contemMaisDeTresDigitos(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+	public void contemMaisDeTresDigitos(FacesContext fc, UIComponent component, Object value)
+			throws ValidatorException {
 		String valor = value.toString();
 		if (valor.length() < 3) {
 			throw new ValidatorException(new FacesMessage("ISBN deve conter mais de 3 dígitos"));
